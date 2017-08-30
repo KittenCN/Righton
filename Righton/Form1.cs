d33a978995fa5bfe64977f747389b2fa0a8c8b5e
Peng Lv <owdely@vip.163.com>
2017-08-30 11:06:43 +0800

    create main form

--- Righton/Form1.cs
+++ Righton/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Righton
 {
-    public partial class Form1 : Form
+    public partial class form_Main : Form
     {
-        public Form1()
+        public form_Main()
         {
             InitializeComponent();
         }
