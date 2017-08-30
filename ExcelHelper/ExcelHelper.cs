dceca963e970a8bdaecf8f2a822b67231aa7188f
Peng Lv <owdely@vip.163.com>
2017-08-30 13:34:21 +0800

    adjust the name of gene

--- ExcelHelper/ExcelHelper.cs
+++ ExcelHelper/ExcelHelper.cs
@@ -24,6 +24,8 @@ namespace ExcelHelper
 
             try
             {
+                string[] strGene = { "ER", "BCL2", "BIR(Survivin)", "MMP11", "ACTB", "GAPD", "GRB7", "AUR（STK15)", "CTSL2", "GST", "ERBB", "MYBL2", "MKI(Ki67)", "PGR", "CD68", "BAG", "CCNB1", "SCUB", "RPL", "TFR", "GUS" };
+
                 if (app == null) return null;
                 workbook = app.Workbooks.Open(filePath, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong,
                     oMissiong, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong);
@@ -38,11 +40,14 @@ namespace ExcelHelper
 
                 //生成行数据
                 //Excel.Range range;
+                int i = 0;
                 for (int iRow = 3; iRow <= iRowCount; iRow++)
                 {
                     int validate = 0;
                     DataRow dr = Result.NewRow();
-                    dr["Gene"] = ((Excel.Range)worksheet.Cells[iRow, 1]).Text;
+                    //dr["Gene"] = ((Excel.Range)worksheet.Cells[iRow, 1]).Text;
+                    dr["Gene"] = strGene[i];
+                    i++;
                     dr["Ct"] = double.Parse(((Excel.Range)worksheet.Cells[iRow, 2]).Text.ToString());
                     if (validate == 0) Result.Rows.Add(dr);
                 }
