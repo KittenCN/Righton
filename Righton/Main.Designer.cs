bd229471d860c34d7a472c66daece97e5712152d
Peng Lv <owdely@vip.163.com>
2017-09-12 16:39:19 +0800

    adjust backgroud pic and ico file

--- Righton/Main.Designer.cs
+++ Righton/Main.Designer.cs
@@ -41,42 +41,44 @@
             this.Ct = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btn_cal = new System.Windows.Forms.Button();
             this.btn_insertData = new System.Windows.Forms.Button();
-            this.pb_foot = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Logo)).BeginInit();
             this.panel_workspace.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_Gene_Process)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_Gene_Ori)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_foot)).BeginInit();
             this.SuspendLayout();
             // 
             // pb_Logo
             // 
             this.pb_Logo.Image = ((System.Drawing.Image)(resources.GetObject("pb_Logo.Image")));
-            this.pb_Logo.Location = new System.Drawing.Point(3, 4);
+            this.pb_Logo.Location = new System.Drawing.Point(0, 0);
+            this.pb_Logo.Margin = new System.Windows.Forms.Padding(0);
             this.pb_Logo.Name = "pb_Logo";
-            this.pb_Logo.Size = new System.Drawing.Size(800, 71);
+            this.pb_Logo.Size = new System.Drawing.Size(1080, 980);
             this.pb_Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pb_Logo.TabIndex = 0;
             this.pb_Logo.TabStop = false;
             // 
             // panel_workspace
             // 
+            this.panel_workspace.BackColor = System.Drawing.SystemColors.Control;
             this.panel_workspace.Controls.Add(this.btn_aboutme);
             this.panel_workspace.Controls.Add(this.lab_Result);
             this.panel_workspace.Controls.Add(this.dgv_Gene_Process);
             this.panel_workspace.Controls.Add(this.dgv_Gene_Ori);
             this.panel_workspace.Controls.Add(this.btn_cal);
             this.panel_workspace.Controls.Add(this.btn_insertData);
-            this.panel_workspace.Location = new System.Drawing.Point(3, 82);
+            this.panel_workspace.Location = new System.Drawing.Point(86, 124);
+            this.panel_workspace.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel_workspace.Name = "panel_workspace";
-            this.panel_workspace.Size = new System.Drawing.Size(809, 674);
+            this.panel_workspace.Size = new System.Drawing.Size(929, 744);
             this.panel_workspace.TabIndex = 1;
             // 
             // btn_aboutme
             // 
-            this.btn_aboutme.Location = new System.Drawing.Point(681, 3);
+            this.btn_aboutme.Location = new System.Drawing.Point(771, 9);
+            this.btn_aboutme.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_aboutme.Name = "btn_aboutme";
-            this.btn_aboutme.Size = new System.Drawing.Size(115, 51);
+            this.btn_aboutme.Size = new System.Drawing.Size(153, 68);
             this.btn_aboutme.TabIndex = 5;
             this.btn_aboutme.Text = "AboutMe";
             this.btn_aboutme.UseVisualStyleBackColor = true;
@@ -88,9 +90,10 @@
             this.lab_Result.AutoSize = true;
             this.lab_Result.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.lab_Result.ForeColor = System.Drawing.Color.Red;
-            this.lab_Result.Location = new System.Drawing.Point(297, 4);
+            this.lab_Result.Location = new System.Drawing.Point(397, 11);
+            this.lab_Result.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lab_Result.Name = "lab_Result";
-            this.lab_Result.Size = new System.Drawing.Size(87, 36);
+            this.lab_Result.Size = new System.Drawing.Size(116, 48);
             this.lab_Result.TabIndex = 4;
             this.lab_Result.Text = "RS=*";
             this.lab_Result.Visible = false;
@@ -104,11 +107,12 @@
             this.dgv_Gene_Process.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn1,
             this.dataGridViewTextBoxColumn2});
-            this.dgv_Gene_Process.Location = new System.Drawing.Point(401, 62);
+            this.dgv_Gene_Process.Location = new System.Drawing.Point(468, 83);
+            this.dgv_Gene_Process.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgv_Gene_Process.Name = "dgv_Gene_Process";
             this.dgv_Gene_Process.ReadOnly = true;
             this.dgv_Gene_Process.RowTemplate.Height = 30;
-            this.dgv_Gene_Process.Size = new System.Drawing.Size(405, 531);
+            this.dgv_Gene_Process.Size = new System.Drawing.Size(456, 650);
             this.dgv_Gene_Process.TabIndex = 3;
             // 
             // dataGridViewTextBoxColumn1
@@ -136,11 +140,12 @@
             this.dgv_Gene_Ori.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Gene,
             this.Ct});
-            this.dgv_Gene_Ori.Location = new System.Drawing.Point(0, 62);
+            this.dgv_Gene_Ori.Location = new System.Drawing.Point(4, 83);
+            this.dgv_Gene_Ori.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgv_Gene_Ori.Name = "dgv_Gene_Ori";
             this.dgv_Gene_Ori.ReadOnly = true;
             this.dgv_Gene_Ori.RowTemplate.Height = 30;
-            this.dgv_Gene_Ori.Size = new System.Drawing.Size(400, 531);
+            this.dgv_Gene_Ori.Size = new System.Drawing.Size(456, 650);
             this.dgv_Gene_Ori.TabIndex = 2;
             // 
             // Gene
@@ -162,9 +167,10 @@
             // btn_cal
             // 
             this.btn_cal.Enabled = false;
-            this.btn_cal.Location = new System.Drawing.Point(131, 4);
+            this.btn_cal.Location = new System.Drawing.Point(165, 9);
+            this.btn_cal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_cal.Name = "btn_cal";
-            this.btn_cal.Size = new System.Drawing.Size(115, 51);
+            this.btn_cal.Size = new System.Drawing.Size(153, 68);
             this.btn_cal.TabIndex = 1;
             this.btn_cal.Text = "Process";
             this.btn_cal.UseVisualStyleBackColor = true;
@@ -172,35 +178,26 @@
             // 
             // btn_insertData
             // 
-            this.btn_insertData.Location = new System.Drawing.Point(10, 4);
+            this.btn_insertData.Location = new System.Drawing.Point(4, 9);
+            this.btn_insertData.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_insertData.Name = "btn_insertData";
-            this.btn_insertData.Size = new System.Drawing.Size(115, 51);
+            this.btn_insertData.Size = new System.Drawing.Size(153, 68);
             this.btn_insertData.TabIndex = 0;
-            this.btn_insertData.Text = "InputExcel";
+            this.btn_insertData.Text = "Data";
             this.btn_insertData.UseVisualStyleBackColor = true;
             this.btn_insertData.Click += new System.EventHandler(this.btn_insertData_Click);
             // 
-            // pb_foot
-            // 
-            this.pb_foot.Image = ((System.Drawing.Image)(resources.GetObject("pb_foot.Image")));
-            this.pb_foot.Location = new System.Drawing.Point(3, 677);
-            this.pb_foot.Name = "pb_foot";
-            this.pb_foot.Size = new System.Drawing.Size(806, 78);
-            this.pb_foot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pb_foot.TabIndex = 2;
-            this.pb_foot.TabStop = false;
-            // 
             // form_Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(811, 758);
-            this.Controls.Add(this.pb_foot);
+            this.ClientSize = new System.Drawing.Size(1076, 977);
             this.Controls.Add(this.panel_workspace);
             this.Controls.Add(this.pb_Logo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "form_Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -210,7 +207,6 @@
             this.panel_workspace.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_Gene_Process)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_Gene_Ori)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_foot)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -225,7 +221,6 @@
         private System.Windows.Forms.DataGridView dgv_Gene_Process;
         private System.Windows.Forms.Label lab_Result;
         private System.Windows.Forms.Button btn_aboutme;
-        private System.Windows.Forms.PictureBox pb_foot;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Gene;
